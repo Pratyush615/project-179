f59473ec6f560d6047c04d26fe6b0a9eaaf0f1a9
Pratyush615 <75452860+Pratyush615@users.noreply.github.com>
2024-02-25 10:25:10 -0500

    Add files via upload

---
+++ ar_navigation.js
@@ -0,0 +1,29 @@
+let coordinates = {
+
+}
+
+$(document).ready(function(){
+    get_coordinates()
+
+})
+
+function get_coordinates(){
+    let search_params = new URLSearchParams(window.location.search)
+
+    if(search_params.has('source')&&search_params.has('destination')){
+        let source = search_params.get('source')
+        let destination = search_params.get('destination')
+
+        coordinates.source_lat = source.split(';')[0]
+        coordinates.source_long = source.split(';')[1]
+
+        coordinates.destination_lat = destination.split(';')[0]
+        coordinates.destination_long = destination.split(';')[1]
+
+        
+    }
+    else{
+        alert('Coordinates not Selected')
+        window.history.back()
+    }
+}
